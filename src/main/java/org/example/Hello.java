82db6a167cef41f1054f74bb8343265d90137fad
ganeshraj1618 <ganeshraj1.618@gmail.com>
2025-08-24 15:11:32 +0530

    init commit

---
+++ src/main/java/org/example/Hello.java
@@ -0,0 +1,16 @@
+public class  Hello
+{
+    public static void main(String args[])
+    {
+        StringBuffer sb = new StringBuffer("Navin");
+        sb.append("Reddy");  //  appending the name reddy to the navin string object in the heap memeory
+
+        sb.insert(5," java ");        //insert java string
+        sb.setLength(30);          // set length to 30 fill in future
+        sb.ensureCapacity(100);          // change capacity
+        sb.deleteCharAt(3);        // delete character
+        System.out.println(sb.capacity());// capacity of the string
+        System.out.println(sb);
+
+    }
+}
