591705023fc3614e04712ba8ee4c65de4148527d
ganeshraj1618 <ganeshraj1.618@gmail.com>
2025-10-02 21:55:24 +0530

    init commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,42 @@ public class Main {
         final byte monthsInYear = 12;
         final byte percent = 100;
         Scanner scanner = new Scanner(System.in);
+        double monthlyRate = 0;
+        double principal = 0;
+        int noOfPayments =0;
 
-        System.out.print("Principle:");
-        double principal = scanner.nextDouble();
+        while (true) {
+            System.out.print("\nPrinciple (1K - 1M):");
+            principal = scanner.nextDouble();
+            if (principal >= 1000 && principal <= 1000000)
+                break;
+            System.out.print("Enter a value between 1,000 and 1,000,000");
+        }
 
-        System.out.print("Annual intrest rate (%) :");
-        double annualRate = scanner.nextDouble();
-        double monthlyRate = annualRate / monthsInYear / percent ;
+        while (true)
+        {
+            System.out.print("\nAnnual intrest rate (%) :");
+            double annualRate = scanner.nextDouble();
+            if (annualRate > 0 && annualRate <= 100)
+            {
+                monthlyRate = annualRate / monthsInYear / percent;
+                break;
+            }
 
-        System.out.print("period (Years):");
-        int years = scanner.nextInt();
-        int noOfPayments = years* monthsInYear ;
+        System.out.print("Enter a value between 0 and 100");
+        }
+
+
+        while (true){
+            System.out.print("\nperiod (Years):");
+            int years = scanner.nextInt();
+            if (years >0 && years <31) {
+                noOfPayments = years * monthsInYear;
+                break;
+            }
+        System.out.println("Enter a value between 1 and 30");
+        }
+
 
         double mortgage = principal * ( monthlyRate * Math.pow( 1 + monthlyRate , noOfPayments ))/( Math.pow( 1 + monthlyRate , noOfPayments ) - 1);
         System.out.print("Mortgage :" + NumberFormat.getCurrencyInstance().format(mortgage));
